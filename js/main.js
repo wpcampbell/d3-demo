e1d0217b81d53a8f504f8097f8f1cf0d018fc63d
wpcampbell <77030540+wpcampbell@users.noreply.github.com>
2021-07-20 10:53:29 -0500

    d3 file fix

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /*Stylesheet by Will P. Campbell,2021*/
 
+//execute script when window is loaded
+window.onload = function(){
+
+    var container = d3.select("body") //get the <body> element from the DOM
+     //Example 1.1 line 3...container block
+     .append("svg") //put a new svg in the body
+
+};
+
+
